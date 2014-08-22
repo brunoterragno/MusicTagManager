bd3ab31395c44cdbd4acbad20e25d502967e5666
Bruno Terragno <bruno.terragno@gmail.com>
2014-08-22 00:26:23 -0300

    Class to list music
    add set to some properties

--- Model/Music.cs
+++ Model/Music.cs
@@ -5,18 +5,18 @@ namespace MusicTagManager.Model
 {
     public class Music
     {
-        private TagLib.File _file;
-        public uint Track { get { return _file.Tag.Track; } set { _file.Tag.Track = value; } }
-        public string Filename { get { return Path.GetFileName(_file.Name); } }
-        public string Tittle { get { return _file.Tag.Title; } set { _file.Tag.Title = value; } }
-        public string AlbumTittle { get { return _file.Tag.Album; } }
-        public string[] AlbumArtists { get { return _file.Tag.AlbumArtists; } }
-        public string[] ContributingArtists { get { return _file.Tag.Artists; } }
-        public uint AlbumYear { get { return _file.Tag.Year; } }
+        public TagLib.File File { get; set; }
+        public uint Track { get { return File.Tag.Track; } set { File.Tag.Track = value; } }
+        public string Filename { get { return Path.GetFileName(File.Name); } }
+        public string Tittle { get { return File.Tag.Title; } set { File.Tag.Title = value; } }
+        public string AlbumTittle { get { return File.Tag.Album; } }
+        public string[] AlbumArtists { get { return File.Tag.AlbumArtists; } }
+        public string[] ContributingArtists { get { return File.Tag.Artists; } }
+        public uint AlbumYear { get { return File.Tag.Year; } }
 
         public Music(TagLib.File file)
         {
-            this._file = file;
+            this.File = file;
         }
     }
 }
